24f685b13f2142a426443d84647003913cf034ac
Jakub Pawlak <kuba_pawlak@icloud.com>
2024-10-29 19:32:19 +0100

    Add tracing and yaeger

--- services/conversion/app.py
+++ services/conversion/app.py
@@ -1,36 +1,38 @@
 import logging
 
 from flask import Flask, render_template, request
+from opentelemetry import trace
+from opentelemetry.trace import Span
 
 from conversion.convert import convert_to_txt
 from conversion.logging import configure_logging
 from conversion.messaging.rabbitmq import publish_message
-
-from opentelemetry import trace
 
 tracer = trace.get_tracer(__name__)
 
 configure_logging()
 app = Flask(__name__)
 
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    logger = logging.getLogger(__name__ + '.index')
-    logger.warning("Dupa %s", 'hehe')
-    if request.method == 'POST':
-        file = request.files['file']
-        if file:
-            file_path = file.filename
-            file.save(file_path)
-            try:
-                text = convert_to_txt(file_path)
-                publish_message(exchange_name='logs', routing_key='', message=text)
-                return f"Converted text: {text}"
-            except Exception as e:
-                logger.error(e)
-                return f"Error: {str(e)}"
-    return render_template('index.html')
+    with tracer.start_as_current_span("DUPA") as span:
+        span: Span
+        span.set_attribute("my_attr", 69)
+        logger = logging.getLogger(__name__ + '.index')
+        logger.warning("Dupa")
+        if request.method == 'POST':
+            file = request.files['file']
+            if file:
+                file_path = file.filename
+                file.save(file_path)
+                try:
+                    text = convert_to_txt(file_path)
+                    publish_message(exchange_name='logs', routing_key='', message=text)
+                    return f"Converted text: {text}"
+                except Exception as e:
+                    logger.error(e)
+                    return f"Error: {str(e)}"
+        return render_template('index.html')
 
 
 if __name__ == '__main__':
