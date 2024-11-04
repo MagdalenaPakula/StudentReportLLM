85cac2cd63d703ceed47ffcae665dfad8366f9ad
Jakub Pawlak <kuba_pawlak@icloud.com>
2024-11-04 17:48:54 +0100

    Add request and conversion metrics

--- services/conversion/app.py
+++ services/conversion/app.py
@@ -1,38 +1,38 @@
 import logging
 
 from flask import Flask, render_template, request
+from flask_http_middleware import MiddlewareManager
 from opentelemetry import trace
-from opentelemetry.trace import Span
 
 from conversion.convert import convert_to_txt
 from conversion.logging import configure_logging
 from conversion.messaging.rabbitmq import publish_message
+from middleware import RequestTimingMiddleware
 
 tracer = trace.get_tracer(__name__)
 
 configure_logging()
 app = Flask(__name__)
+app.wsgi_app = MiddlewareManager(app)
+app.wsgi_app.add_middleware(RequestTimingMiddleware)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    with tracer.start_as_current_span("DUPA") as span:
-        span: Span
-        span.set_attribute("my_attr", 69)
-        logger = logging.getLogger(__name__ + '.index')
-        logger.warning("Dupa")
-        if request.method == 'POST':
-            file = request.files['file']
-            if file:
-                file_path = file.filename
-                file.save(file_path)
-                try:
-                    text = convert_to_txt(file_path)
-                    publish_message(exchange_name='logs', routing_key='', message=text)
-                    return f"Converted text: {text}"
-                except Exception as e:
-                    logger.error(e)
-                    return f"Error: {str(e)}"
-        return render_template('index.html')
+    logger = logging.getLogger(__name__ + '.index')
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            file_path = file.filename
+            file.save(file_path)
+            try:
+                text = convert_to_txt(file_path)
+                publish_message(exchange_name='logs', routing_key='', message=text)
+                return f"Converted text: {text}"
+            except Exception as e:
+                logger.error(e)
+                return f"Error: {str(e)}"
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
